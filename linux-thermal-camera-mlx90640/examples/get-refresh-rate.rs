97cc8223ff2901f1359ab8a814e251649cee960c
franc <francesco.gramazio17@gmail.com>
2025-03-08 18:51:43 +1100

    some improvements

---
+++ linux-thermal-camera-mlx90640/examples/get-refresh-rate.rs
@@ -0,0 +1,22 @@
+﻿use linux_thermal_camera_mlx90640::ThermalCamera;
+
+fn main() {
+    println!("and here we go, doing awesome things in rust ! ;D");
+    let mut thermal_camera = ThermalCamera::new(0x33, 1);
+
+    println!("i2c bus initialised successfully");
+
+    let refresh_rate = thermal_camera.get_refresh_rate();
+
+    println!("current refresh rate {:?}", refresh_rate);
+
+    // println!("trying to get the image");
+
+    // let image = thermal_camera.get_image();
+
+    // for pixel in image{
+
+    //     println!("{}", pixel)
+
+    // }
+}
--- linux-thermal-camera-mlx90640/examples/test.rs
+++
@@ -1,5 +0,0 @@
-use linux_thermal_camera_mlx90640::ThermalCamera;
-
-fn main() {
-    ThermalCamera::new(0x33, 1);
-}
